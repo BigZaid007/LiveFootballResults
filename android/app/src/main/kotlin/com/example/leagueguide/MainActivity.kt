7f0805ff368f5987351a45f4faf207cba1308a60
Zbook15 <Zaid.h.qassim93z@gmail.com>
2020-11-15 23:10:16 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/leagueguide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.leagueguide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+import 'package:leagueguide/Widgets/LeagueContainer.dart';
+
+import 'TableScreen.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.deepOrangeAccent),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ListView(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Leagues Guide',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                  children: [
+                    GestureDetector(
+                      child: LeagueContainer(image: 'assets/pl.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'PL'),
+                            ));
+                      },
+                    ),
+                    GestureDetector(
+                      child: LeagueContainer(image: 'assets/laliga.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'PD'),
+                            ));
+                      },
+                    ),
+                    GestureDetector(
+                      child: LeagueContainer(image: 'assets/bundesliga.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'BL1'),
+                            ));
+                      },
+                    ),
+                    GestureDetector(
+                      child: LeagueContainer(image: 'assets/seria.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'SA'),
+                            ));
+                      },
+                    ),
+                    GestureDetector(
+                      child: LeagueContainer(image: 'assets/ligue1.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'FL1'),
+                            ));
+                      },
+                    ),
+                    GestureDetector(
+                      child: LeagueContainer(image: 'assets/nos.png'),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TableScreen(code: 'PPL'),
+                            ));
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
